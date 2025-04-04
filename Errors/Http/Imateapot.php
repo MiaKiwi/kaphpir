03504228e5587bc086d0b6b8fc1a0369ee5cc1c0
Mia Delan <deland@mia.kiwi>
2025-04-04 22:38:42 +0200

    Fixed HTTP error 418

--- Errors/Http/Imateapot.php
+++ Errors/Http/Imateapot.php
@@ -6,8 +6,8 @@ use MiaKiwi\Kaphpir\Errors\Http\HttpError;
 
 class ImATeapot extends HttpError
 {
-    public function __construct(string $message = 'I’m a teapot', array $previous = [])
+    public function __construct(string $message = 'I\'m a teapot', array $previous = [])
     {
-        parent::__construct(418, 'I___M_A_TEAPOT', $message, $previous);
+        parent::__construct(418, 'I_M_A_TEAPOT', $message, $previous);
     }
 }
